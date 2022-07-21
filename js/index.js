05bdc63f60806293fd11a537f53b706b2695890a
techEmprez <suigeneriz001@gmail.com>
2022-07-21 19:23:07 +0100

    Remove unneccesary comments

--- js/index.js
+++ js/index.js
@@ -1,84 +1,84 @@
-import utils from './utils.js';
-import projects from './projects.js';
-import createModal from './modal.js';
+import utils from "./utils.js";
+import projects from "./projects.js";
+import createModal from "./modal.js";
 
-const burgerIcon = utils.qs('#main-header li');
-const burgerCloseIcon = utils.qsa('#main-header li')[1];
-const container = utils.qs('#main-header ul');
-const links = utils.qsa('li.desktop-visible', container);
+const burgerIcon = utils.qs("#main-header li");
+const burgerCloseIcon = utils.qsa("#main-header li")[1];
+const container = utils.qs("#main-header ul");
+const links = utils.qsa("li.desktop-visible", container);
 
 function toggleBurgerMenu() {
-  burgerIcon.classList.toggle('no-display');
-  burgerCloseIcon.classList.toggle('no-display');
-  container.classList.toggle('enlarged');
+  burgerIcon.classList.toggle("no-display");
+  burgerCloseIcon.classList.toggle("no-display");
+  container.classList.toggle("enlarged");
 
   for (let i = 0, l = links.length; i < l; i += 1) {
-    links[i].classList.toggle('desktop-visible');
+    links[i].classList.toggle("desktop-visible");
   }
 }
 
-burgerIcon.addEventListener('click', () => toggleBurgerMenu());
-burgerCloseIcon.addEventListener('click', () => toggleBurgerMenu());
+burgerIcon.addEventListener("click", () => toggleBurgerMenu());
+burgerCloseIcon.addEventListener("click", () => toggleBurgerMenu());
 
-container.addEventListener('click', (e) => {
+container.addEventListener("click", (e) => {
   const { target } = e;
 
-  if (target.tagName.toLowerCase() !== 'a') return;
+  if (target.tagName.toLowerCase() !== "a") return;
 
   toggleBurgerMenu();
-  utils.qs(target.getAttribute('href')).scrollIntoView();
+  utils.qs(target.getAttribute("href")).scrollIntoView();
 });
 
 // If the burger menu is opened and the screen is resized to desktop
 // dimensions, close the menu
 window.onresize = () => {
-  if (window.innerWidth >= 768 && container.classList.contains('enlarged')) {
+  if (window.innerWidth >= 768 && container.classList.contains("enlarged")) {
     toggleBurgerMenu();
   }
 };
 
 // Create and insert articles in the page
 function populateProjects() {
-  const projectsContainer = utils.qs('#projects .grid-container');
+  const projectsContainer = utils.qs("#projects .grid-container");
 
   projects.forEach((project) => {
     const article = utils.createElement({
-      tagName: 'article',
+      tagName: "article",
       id: `art${project.id}`,
     });
 
     if (project.invisible) {
-      article.classList.add('mobile-visible');
+      article.classList.add("mobile-visible");
     }
 
     article.appendChild(
       utils.createElement({
-        tagName: 'img',
+        tagName: "img",
         src: `img/projects/${project.image}`,
         alt: `Laptop project-${project.id} image`,
       })
     );
 
     const div = utils.createElement({
-      class: 'info',
+      class: "info",
     });
 
     div.appendChild(
       utils.createElement({
-        tagName: 'h2',
+        tagName: "h2",
         textContent: project.title,
       })
     );
 
-    const ul = utils.createElement({ tagName: 'ul' });
+    const ul = utils.createElement({ tagName: "ul" });
 
     project.languages.forEach((language) => {
-      const li = utils.createElement({ tagName: 'li' });
+      const li = utils.createElement({ tagName: "li" });
       li.appendChild(
         utils.createElement({
-          tagName: 'a',
-          href: '#',
-          class: 'tag',
+          tagName: "a",
+          href: "#",
+          class: "tag",
           textContent: language,
         })
       );
@@ -87,17 +87,17 @@ function populateProjects() {
     div.appendChild(ul);
 
     const a = utils.createElement({
-      tagName: 'a',
-      href: '#',
-      class: ['button', 'secondary-dark'],
-      textContent: 'See this project ',
+      tagName: "a",
+      href: "#",
+      class: ["button", "secondary-dark"],
+      textContent: "See this project ",
     });
     a.appendChild(
       utils.createElement({
-        tagName: 'object',
-        data: 'img/svgs/arrow.svg',
-        width: '10',
-        height: '10',
+        tagName: "object",
+        data: "img/svgs/arrow.svg",
+        width: "10",
+        height: "10",
       })
     );
     div.appendChild(a);
@@ -110,69 +110,66 @@ function populateProjects() {
 populateProjects();
 
 // Listen for the click event fired by the project buttons
-utils.qs('#projects').addEventListener('click', (e) => {
+utils.qs("#projects").addEventListener("click", (e) => {
   e.preventDefault();
 
   const { target } = e;
 
-  if (!target.classList.contains('button')) return;
+  if (!target.classList.contains("button")) return;
 
-  const article = target.closest('article');
-  const articleTitle = utils.qs('h2', article).textContent;
+  const article = target.closest("article");
+  const articleTitle = utils.qs("h2", article).textContent;
   const project = projects.find((p) => p.title === articleTitle);
 
   createModal(project);
 });
 
 // Check form for improper input (not lowercase)
-utils.qs('form').addEventListener('submit', (e) => {
+utils.qs("form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const { value } = utils.qs('#user_email');
-  const errorField = utils.qs('.emailError');
+  const { value } = utils.qs("#user_email");
+  const errorField = utils.qs(".emailError");
 
   if (value !== value.toLowerCase()) {
-    errorField.textContent = 'Please type your email address in all lowercase.';
+    errorField.textContent = "Please type your email address in all lowercase.";
     return;
   }
 
-  errorField.textContent = '';
-  utils.qs('form').submit();
+  errorField.textContent = "";
+  utils.qs("form").submit();
 });
 
 // Store input data inside an object that facilitates the communication
 // with the LocalStorage so that it can be retrieved for pre-filling
 // purposes
 const store = {
-  name: '',
-  email: '',
-  message: '',
+  name: "",
+  email: "",
+  message: "",
 };
 
-// Some comment no one needs
-utils.qs('#username').addEventListener('input', (e) => {
+utils.qs("#username").addEventListener("input", (e) => {
   store.name = e.target.value;
 });
 
-// Another comment no one needs
-utils.qs('#user_email').addEventListener('input', (e) => {
+utils.qs("#user_email").addEventListener("input", (e) => {
   store.email = e.target.value;
 });
 
-// Let's hope Tracy will do fine. We'll see tomorrow morning ^_^
-utils.qs('#message').addEventListener('input', (e) => {
+utils.qs("#message").addEventListener("input", (e) => {
   store.message = e.target.value;
 });
 
-qsa('#username, #user_email, #message').addEventListener('input', (e) => {
-  localStorage.setItem('data', JSON.stringify(store));
+qsa("#username, #user_email, #message").addEventListener("input", (e) => {
+  localStorage.setItem("data", JSON.stringify(store));
 });
 
-let data = localStorage.getItem('data');
+let data = localStorage.getItem("data");
 
 if (data) {
   data = JSON.parse(data);
-  qs('#username').value = data.name;
-  qs('#user_email').value = data.email;
-  qs('#message').value = data.message;
+  qs("#username").value = data.name;
+  qs("#user_email").value = data.email;
+  qs("#message").value = data.message;
 }
